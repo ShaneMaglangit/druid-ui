43243579966c4ee2b4fcded359471df674698998
ShaneMaglangit <shanedmaglangit@gmail.com>
2024-06-08 12:12:21 +0800

    Add primary and danger buttons

--- src/Button.stories.tsx
+++ src/Button.stories.tsx
@@ -15,11 +15,63 @@ const meta = {
 type Story = StoryObj<typeof meta>;
 
 export const Default = {
+  args: { variant: "default" },
   render: (args: Story) => (
-    <>
+    <div className="flex gap-1">
       <Button {...args} />
       <Button disabled {...args} />
-    </>
+    </div>
+  ),
+};
+
+export const Primary = {
+  args: { variant: "primary" },
+  render: (args: Story) => (
+    <div className="flex gap-1">
+      <Button {...args} />
+      <Button disabled {...args} />
+    </div>
+  ),
+};
+
+export const Danger = {
+  args: { variant: "danger" },
+  render: (args: Story) => (
+    <div className="flex gap-1">
+      <Button {...args} />
+      <Button disabled {...args} />
+    </div>
+  ),
+};
+
+export const WithIcon = {
+  args: {
+    children: (
+      <>
+        <svg
+          className="h-4 w-4"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M5 13l4 4L19 7"
+          />
+        </svg>
+        Click me
+      </>
+    ),
+  },
+  render: (args: Story) => (
+    <div className="flex gap-1">
+      <Button {...args} />
+      <Button {...args} variant="primary" />
+      <Button {...args} variant="danger" />
+    </div>
   ),
 };
 
--- src/Button.tsx
+++ src/Button.tsx
@@ -2,14 +2,21 @@ import { ButtonHTMLAttributes, forwardRef } from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, VariantProps } from "class-variance-authority";
 import { cn } from "@druid-ui/util.ts";
+import { clsx } from "clsx";
 
 const buttonVariants = cva(
-  "focus-visible:ring-ring inline-flex items-center justify-center rounded-md text-sm font-medium focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50",
+  clsx(
+    "inline-flex items-center justify-center gap-1",
+    "text-sm font-medium",
+    "focus-visible:ring-ring rounded-md focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50",
+  ),
   {
     variants: {
       variant: {
         default:
           "text-black hover:bg-gray-100 dark:bg-gray-900 dark:text-white dark:hover:bg-gray-800",
+        primary: "bg-primary-500 text-white hover:bg-primary-800",
+        danger: "bg-danger-500 text-white hover:bg-danger-800",
       },
       size: {
         default: "h-9 px-3 py-1",
